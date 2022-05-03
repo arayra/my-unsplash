75ce9c860c29de4421dbc526627f4de805fa3a4b
irlanvila <irlanvila@outlook.com>
2022-05-03 23:44:56 +0700

    fix responsive when zooming out

--- src/App.vue
+++ src/App.vue
@@ -24,71 +24,73 @@ const images = [
 </script>
 
 <template>
-  <nav
-    class="fixed top-0 left-0 w-full bg-white grid grid-cols-12 font-poppins py-6 px-6 md:text-sm"
-    id="images"
-  >
-    <div
-      class="flex justify-start md:justify-center items-center logo col-span-6 md:col-span-2"
+  <div class="max-w-screen-xl mx-auto">
+    <nav
+      class="fixed top-0 w-full max-w-screen-xl bg-white grid grid-cols-12 font-poppins py-6 px-6 md:text-sm"
+      id="images"
     >
-      <img src="./assets/my_unsplash_logo.svg" alt="My Unsplash Logo" />
-    </div>
-    <div
-      class="flex justify-end md:justify-start items-center font-poppins col-span-6 md:col-span-7 lg:col-span-8 px-4"
-      id="search"
-    >
-      <input
-        type="search"
-        placeholder="Search by name"
-        class="px-3 py-1.5 text-gray-700 w-full md:w-60 bg-white border-2 border-solid border-gray-300 transition-[width] duration-200 focus:border-green-600 focus:outline-none focus:w-full rounded"
-      />
-    </div>
-    <div
-      class="hidden md:flex justify-center items-center col-span-3 lg:col-span-2 w-full"
-      id="add-photo"
-    >
-      <button
-        class="text-white bg-green-600 hover:bg-green-700 transition rounded py-2 px-8"
+      <div
+        class="flex justify-start md:justify-center items-center logo col-span-6 md:col-span-2"
       >
-        Add a photo
-      </button>
-      <Teleport to="body">
+        <img src="./assets/my_unsplash_logo.svg" alt="My Unsplash Logo" />
+      </div>
+      <div
+        class="flex justify-end md:justify-start items-center font-poppins col-span-6 md:col-span-7 lg:col-span-8 px-4"
+        id="search"
+      >
+        <input
+          type="search"
+          placeholder="Search by name"
+          class="px-3 py-1.5 text-gray-700 w-full md:w-60 bg-white border-2 border-solid border-gray-300 transition-[width] duration-200 focus:border-green-600 focus:outline-none focus:w-full rounded"
+        />
+      </div>
+      <div
+        class="hidden md:flex justify-center items-center col-span-3 lg:col-span-2 w-full"
+        id="add-photo"
+      >
         <button
-          class="fixed right-8 bottom-8 rounded-full py-4 px-4 bg-green-600 md:hidden"
+          class="text-white bg-green-600 hover:bg-green-700 transition rounded py-2 px-8"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#ffffff"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-plus"
-          >
-            <line x1="12" y1="5" x2="12" y2="19"></line>
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-          </svg>
+          Add a photo
         </button>
-      </Teleport>
-    </div>
-  </nav>
-  <section class="flex flex-wrap mt-20" id="images">
-    <div
-      class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 h-full p-5"
-      v-for="image in images"
-      :key="image.link"
-    >
-      <img
-        class="rounded-lg object-cover h-80 w-full mx-auto"
-        :src="image.link"
-        :alt="image.name"
-      />
-    </div>
-  </section>
-  <div id="pop-up-box"></div>
+        <Teleport to="body">
+          <button
+            class="fixed right-8 bottom-8 rounded-full py-4 px-4 bg-green-600 md:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-plus"
+            >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+          </button>
+        </Teleport>
+      </div>
+    </nav>
+    <section class="flex flex-wrap mt-20" id="images">
+      <div
+        class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 h-full p-5"
+        v-for="image in images"
+        :key="image.link"
+      >
+        <img
+          class="rounded-lg object-cover h-80 w-full mx-auto"
+          :src="image.link"
+          :alt="image.name"
+        />
+      </div>
+    </section>
+    <div id="pop-up-box"></div>
+  </div>
 </template>
 
 <style>
